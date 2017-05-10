ec954e4532f0ad2729572681768794237f68b8f8
cheekiam <cheekiam@camtech.com.sg>
2017-05-10 21:49:22 +0800

    commit changes course module 8

---
+++ buildScripts/endMessage.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Server running at localhost:3000...'));
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Server running at localhost:3000...'));
+
