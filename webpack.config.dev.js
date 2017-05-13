690284d98ff3a16bbe8ea06225586c0f21ed347f
cheekiam <cheekiam@camtech.com.sg>
2017-05-14 03:42:28 +0800

    build script with webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,47 +1,42 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'inline-source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor:path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'src'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
-  plugins: [],
+  plugins: [
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    //use commonsChunkPlugin to create a separatebundle
+    //of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    //Create HTML file that includes refrence to bundles JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-      { test: /\.css$/, loaders: ['style', 'css'] }
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
-
-// import path from 'path';
-
-// export default {
-//   debug: true,
-//   devtool: 'inline-source-map',
-//   noInfo: false,
-//   entry: [
-//     path.resolve(__dirname, 'src/index')
-//   ],
-//   target: 'web',
-//   output: {
-//     path: path.resolve(__dirname, 'src'),
-//     publicPath: '/',
-//     filename: 'bundle.js'
-//   },
-//   plugins: [],
-//   module: {
-//     loaders: [
-//       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-//       {test: /\.css$/, loaders: ['style','css']}
-//     ]
-//   }
-// }
